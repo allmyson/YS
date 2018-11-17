12f3641fa12440074dd7f6706384a657066a4c4c
allmyson <cqlh5201314>
2018-11-17 21:28:02 +0800

    test home

---
+++ app/src/main/java/com/ys/game/http/BaseHttp.java
@@ -0,0 +1,11 @@
+package com.ys.game.http;
+
+public class BaseHttp {
+    private static BaseHttp instance;
+    public static BaseHttp getInstance(){
+        if(instance == null){
+            instance = new BaseHttp();
+        }
+        return instance;
+    }
+}
